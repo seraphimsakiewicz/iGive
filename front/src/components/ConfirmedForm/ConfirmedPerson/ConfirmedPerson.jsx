7cb32ca621a2f334696532d9382ae611e26a7969
seraphim <seraphimsakiewicz@gmail.com>
2021-11-21 10:09:04 +0300

    form progress

--- front/src/components/ConfirmedForm/ConfirmedPerson/ConfirmedPerson.jsx
+++ front/src/components/ConfirmedForm/ConfirmedPerson/ConfirmedPerson.jsx
@@ -1,4 +1,3 @@
-
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { editConfirmCount } from "../../../redux/ac/confirmedAC";
@@ -10,7 +9,8 @@ export default function ConfirmedPerson({ confirmed }) {
 
   const handleOnChange = (e) => {
     setLiters(e.target.value);
-    const mL = parseInt(liters);
+
+    const mL = +e.target.value;
     dispatch(editConfirmCount(confirmed.id, mL));
   };
   return (
@@ -19,7 +19,7 @@ export default function ConfirmedPerson({ confirmed }) {
       <input
         type="number"
         min="0"
-        step="10"
+        step="20"
         max="1000"
         placeholder="quantity in mL"
         value={liters}
